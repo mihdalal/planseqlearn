9a9de4ac8b1e77874cb91cd533d17ea593b72366
Murtaza Dalal <mdalal@andrew.cmu.edu>
2023-12-04 14:28:35 -0500

    bugfixes for installing and running psl

---
+++ install.sh
@@ -0,0 +1,16 @@
+pip install -e d4rl/
+pip install -e mjrl/
+pip install -e metaworld/
+pip install -e robosuite/
+pip install -e doodad/
+pip install -e mopa-rl-1/
+pip install -e DisentangledRep4RL/
+pip install -e rlkit/
+pip install -r requirements.txt
+pip install dm-env
+pip install distracting-control
+pip install --upgrade dm-control
+pip install mujoco==2.3.5
+pip install numpy==1.23.5
+pip install --upgrade networkx # for removing annoying warning
+echo "/home/mdalal/ompl/ompl-1.5.2/py-bindings" >> ~/mambaforge/envs/planseqlearn/lib/python3.8/site-packages/ompl.pth
