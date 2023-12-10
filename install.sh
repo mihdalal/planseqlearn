66d7d1f1d74b2ae70a2c922868a6d5faa0df988a
Murtaza Dalal <mdalal@andrew.cmu.edu>
2023-12-09 19:30:08 -0500

    updated reqs for kitchen

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ pip install distracting-control
 pip install --upgrade dm-control
 pip install mujoco==2.3.5
 pip install numpy==1.23.5
+pip install mujoco-py==2.0.2.5
 pip install --upgrade networkx # for removing annoying warning
 echo "/home/mdalal/ompl/ompl-1.5.2/py-bindings" >> ~/mambaforge/envs/planseqlearn/lib/python3.8/site-packages/ompl.pth
 pip install -e .
