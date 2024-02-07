bf962d01520bd76284021ce8898c458ee1ff915d
Tarun Chiruvolu <tarunchiruvolu@gmail.com>
2024-02-07 11:36:45 -0500

    text plan kitchen update

--- planseqlearn/psl/env_text_plans.py
+++ planseqlearn/psl/env_text_plans.py
@@ -95,4 +95,11 @@ KITCHEN_PLANS = {
     "kitchen-tlb-v0": [
         ("top left burner", "grasp")
     ]
+    "microwave-kettle-light-burner-slider-v0": [
+        ("microwave handle", "grasp"),
+        ("kettle", "grasp"),
+        ("light", "grasp"),
+        ("light", "grasp"),
+        ("top left burner", "grasp")
+    ]
 }
